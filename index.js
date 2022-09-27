e0f9e4c46027c42a68bbb7b8e46a54e8a3d57cec
Daisy Yi <daisyyi@rocketmail.com>
2022-09-27 12:53:53 -0700

    commit tree da52680fd17edbee08dc2aa16e8cb5a947e5212a

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+function createEmployeeRecord(first, family, titles, pay) {
+    const employeeRecord = {
+      firstName: first,
+      familyName: family,
+      title: titles,
+      payPerPeriod: pay,
+      timeInEvents: [],
+      timeOutEvents: [],
+    }
+    createEmployeeRecords(employeeRecord);
+  
+    console.log(employeeRecord);
+  }
+  
+  createEmployeeRecord("Daisy", "Yi", "boss", 1)
+  
+  function createEmployeeRecords(array) {
+    const employeeRecords = {}
+  }
+  
+  function createTimeOutEvent(record, date, hour) {
+    record.type = "Timeout",
+      record.hour = "HHMM",
+      record.date = "YYYY-MM-DD",
+      console.log(record);
+  }
+  
+  createTimeOutEvent(employeeRecord, "2014-02-28 1400");
